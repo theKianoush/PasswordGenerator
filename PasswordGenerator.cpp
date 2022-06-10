2febabf98b3d88c620e25cf3cd1ffb459b925fd7
theKianoush <keyonranjbar@aol.com>
2022-06-10 03:15:27 -0600

    bug fix 2.0

--- PasswordGenerator.cpp
+++ PasswordGenerator.cpp
@@ -9,15 +9,20 @@ using namespace std;
 int main() {
 
      srand(time(0));     // seeding for rand()
-     
+
 
      // all data
-     string data = "abcdefghijklmnopqrstuvwxyzABCDEFHGIJKLMNOPQRSTUVWXYZ";
-     string pass;
+     string data = "abcdefghijklmnopqrstuvwxyz"
+                    "ABCDEFHGIJKLMNOPQRSTUVWXYZ"
+                    "1234567890"
+                    "`~!@#$%^&*()_+-=[]{}|:;<>?,./";
+     string pass = "0";
 
-     int x ;
+     int x = 0;
 
      bool loop = true;
+
+     system("Color A");
 
      while (loop) {
 
@@ -25,9 +30,9 @@ int main() {
           cout << "Enter Length of password: ";
           cin >> x;
 
+          system("cls");
 
-
-          for (int i = 1; i < x; i++) {
+          for (int i = 1; i <= x; i++) {
                pass = pass + data[rand() % 93]; // getting generated char
 
 
@@ -37,18 +42,19 @@ int main() {
           cout << "Your randomly generated password is : " << pass << "\n\n";
 
 
-          string zero; 
-          cout << "\nPress any key to generate another \nPress 0 to quit \n. . .\n\n";
+          string zero;
+          cout << "\nEnter any key to generate another \nPress 0 to quit \n. . .\n\n";
           cin >> zero;
           if (zero == "0") {
-          
+
                loop = false;
           }
 
+          pass = " ";
           cout << "\n-------------------------------------------------------------------------------\n";
      }
-           
-          
+
+
 
      return 0;
 }
